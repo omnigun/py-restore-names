b7f7d28fd65f4ab136a6b507f6fa13e0b6d3f80f
Yuriy Komar <omnigun85@gmail.com>
2024-11-08 14:13:21 +0200

    'Solution'

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -1,4 +1,18 @@
 import pytest
 from app.restore_names import restore_names
 
-# write your tests here
+
+@pytest.fixture()
+def user_template() -> dict:
+    return {"last_name": "Smith", "full_name": "Bob Smith"}
+
+
+def test_restore_names_with_none(user_template: dict) -> None:
+    user_template["first_name"] = None
+    restore_names([user_template])
+    assert user_template["first_name"] == user_template["full_name"].split()[0]
+
+
+def test_restore_names_with_empty(user_template: dict) -> None:
+    restore_names([user_template])
+    assert user_template["first_name"] == user_template["full_name"].split()[0]
